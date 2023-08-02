bb6cc8873d3f4b52db03944fe4af4dec61852126
= <=>
2023-08-02 13:09:42 +0530

    optimised code for mobile view

--- cypress/e2e/createOrder.cy.js
+++ cypress/e2e/createOrder.cy.js
@@ -13,3 +13,23 @@ describe("create order end to end scenario",()=> {
         cy.fillPaymentDetailsAndCompletePayment()
     })
 })
+
+/**
+ * Check functionality for mobile view
+ */
+describe("create order end to end scenario for mobile view",()=> {
+    beforeEach("login",()=> {
+        cy.loginWithvalidCredentials()
+        cy.viewport("iphone-6+")
+    })
+    /**
+     * End To End Scenario: Placed Order
+     */
+
+    it("create order",()=> {
+        cy.selectProductToBuy()
+        cy.placeOrder()
+        cy.fillAllDetailsToProcessedPaymentPage()
+        cy.fillPaymentDetailsAndCompletePayment()
+    })
+})
--- cypress/e2e/loginPageFunctionality.cy.js
+++ cypress/e2e/loginPageFunctionality.cy.js
@@ -14,3 +14,27 @@ describe("Verify Login Functionality", ()=> {
         cy.loginWithValidCredentials()
     })
 })
+
+/**
+ * Verify login functionality in mobile view
+ */
+describe ("Login functionality in mobile view",()=> {
+    beforeEach(()=> {
+        cy.viewport("samsung-s10")
+    })
+
+
+
+it ("login in tho the application with Invalid user name and password", ()=> {
+    cy.loginWithInvalidCredentials()
+})
+
+
+it ("Verify the functionality of cancel button", ()=> {
+    cy.verifyFunctionalityOfCancelButton()
+})
+
+it ("login in tho the application with valid user name and password", ()=> {
+    cy.loginWithValidCredentials()
+})
+})
